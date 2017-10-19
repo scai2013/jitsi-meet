855443bc81c5ecce200dc7d9484c9e03dc345243
paweldomas <pawel.domas@jitsi.org>
2017-10-19 13:19:32 -0500

    ref(base/react/Container): return null for not visible
    Now that we no longer hide the filmstrip it is fine to return null
    instead of trying to hide with some tricks.

--- react/features/base/react/components/native/Container.js
+++ react/features/base/react/components/native/Container.js
@@ -7,7 +7,6 @@ import {
     View
 } from 'react-native';
 
-import { Platform } from '../../';
 import AbstractContainer from '../AbstractContainer';
 
 /**
@@ -39,19 +38,8 @@ export default class Container extends AbstractContainer {
 
         // visible
         if (!visible) {
-            // FIXME: Whatever I try ends up failing somehow on Android, give up
-            // for now, hoping display: 'none' solves this.
-            if (Platform.OS === 'android') {
-                return null;
-            }
-
-            // Intentionally hide this Container without destroying it.
             // TODO Replace with display: 'none' supported in RN >= 0.43.
-            props.style = {
-                ...props.style,
-                height: 0,
-                width: 0
-            };
+            return null;
         }
 
         let element = super._render(View, props);
